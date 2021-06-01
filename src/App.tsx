7f492ddfa6f8bf0f1d4a1afd6d26e47f5713c05d
kuro-channel <takumikuroiwa@gmail.com>
2021-06-01 14:16:20 +0900

    20210601_追加 ・タイマーアプリ追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Counter from './Counter';
+import Timer from './Timer';
 import './App.css';
 
 const App: React.FC = () => (
   <div className="container">
-    <header>
-      <h1>カウンター</h1>
-    </header>
+    <h1>カウンター</h1>
     <Counter />
+    <h1>タイマー</h1>
+    <Timer limit={60} />
   </div>
 );
 
